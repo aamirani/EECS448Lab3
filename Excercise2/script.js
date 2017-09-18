43a36b4a71e472e91f0de97afd6f6d8750cfb732
Armaan Amirani <armaanamirani@gmail.com>
2017-09-18 14:07:36 -0500

    Next button function completed

--- Excercise2/script.js
+++ Excercise2/script.js
@@ -1,6 +1,11 @@
-var img = document.getElementsByTagName('img');
-var imgarray = [] //stores img URL
+var imgarray = ["img/img1.jpg","img/img2.jpg","img/img3.jpg","img/img4.jpg","img/img5.jpg"] //stores img URL
 var index = 0; //Variable to store index of imgarray
 function next(){ //Gets next image
-
+    if(index == imgarray.length-1){
+        index = 0;
+    }
+    else{
+        index++;
+    }
+    document.getElementById('ss').src = imgarray[index];
 }
